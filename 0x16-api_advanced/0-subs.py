ef942aaf6fc9361241ad39ceca3466a376f9ea18
Dhulkifli Abbas <dhulkifliabbas06@gmail.com>
2023-01-31 11:50:56 +0300

    Task 0 done

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Contains number_of_subscribers function"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Queries the Reddit API and
+    returns the number of subscribers for a given subreddit
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'my-custom-user-agent'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
